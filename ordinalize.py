639559ac1fb10fbc5003bfd2a12753297ef2d637
Soomin Song <100112940+nimoooos@users.noreply.github.com>
2023-08-14 14:57:30 -1000

    Update ordinalize.py
    Simplified logic, implemented emoji response for 1~3

--- ordinalize.py
+++ ordinalize.py
@@ -1,11 +1,16 @@
 def num_to_ordinal(number):
+    """
+    Convert number to their ordinal value for placement purposes.
+    Functions improperly if number>100, and will return "101th", "102th", etc.
+    """
     last_digit = number % 10
-    is_teen = False
-    if 11 >= number % 100 >= 19:
-        is_teen = True
+    if number == 1: return str("🥇")
+    if number == 2: return str("🥈")
+    if number == 3: return str("🥉")
 
-    if is_teen or last_digit >= 4: return str(number + "th")
+    if number <= 20: return str(number + "th")
+
     if last_digit == 1: return str(number + "st")
     if last_digit == 2: return str(number + "nd")
     if last_digit == 3: return str(number + "rd")
-
+    else: return str(number + "th")
