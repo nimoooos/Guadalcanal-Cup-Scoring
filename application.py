2d48f2f6918cfc598c5f7332111cf79ea9fd63e6
Soomin Song <100112940+nimoooos@users.noreply.github.com>
2023-09-20 11:28:52 -1000

    Updating readability in application.py
    Added type annotations, comments, and renamed variables for readability
    Created make_scoreboard_mini() to modularize request handling
    index.html hides leaderboard when mini_scoreboard is being displayed

--- application.py
+++ application.py
@@ -20,10 +20,10 @@ models.connect_db(app)
 app.app_context().push()
 models.db.create_all()
 
-scoreboard_global = []
-scoreboard_global_pivot = []
-scoreboard_update_time = "N/A"
-print("Scoreboard: ", scoreboard_update_time)
+# these lists hold the data for scoreboard to reduce database access
+scoreboard_global: list[list] = []
+scoreboard_global_pivot: list[list] = []
+scoreboard_update_time: datetime.datetime = datetime.datetime(year=1970, month=1, day=1)  # using 1970 to align with unix time 0
 
 
 def update_scoreboard() -> None:
@@ -34,18 +34,17 @@ def update_scoreboard() -> None:
     events = models.Event.query.order_by(models.Event.id).all()  # list of all events, ordered by id
     events.pop(0)  # remove admin from events
 
-    for t in teams:
-        t.update_score()
+    for team in teams:
+        team.update_score()
 
     # create header for scoreboard table
-    header = ["Teams"]
-    for e in events:
-        header.append(e.name)
+    header: list[str] = ["Teams"]
+    for event in events:
+        header.append(event.name)
     header.append("Total Score")
-
     scoreboard = [header]  # create scoreboard table with header row
-    for team in teams:
-        # loop generates a row with team name and scores from each event
+
+    for team in teams:  # each loop generates a new row with team name and scores from each event
         row = [team.name]  # generate a row with team name as first item
 
         # query with all the team's placements, ordered by event id
@@ -54,13 +53,15 @@ def update_scoreboard() -> None:
             if placement.place == 0:
                 row.append("N/A")
             else:
-                row.append(models.place_to_score(placement.place) *  # raw score from placement
-                           models.Event.query.filter_by(id=placement.events_id).first().weight)  # weight from event
+                raw_score = models.place_to_score(placement.place)
+                score_weight = models.Event.query.filter_by(id=placement.events_id).first().weight
+                row.append(raw_score*score_weight)
 
         row.append(team.score)  # total score for each team at the end of row
         scoreboard.append(row)  # add the created row to scoreboard
 
     global scoreboard_global
+    scoreboard[0][0] = ""
     scoreboard_global = scoreboard
 
     global scoreboard_global_pivot
@@ -69,7 +70,8 @@ def update_scoreboard() -> None:
     global scoreboard_update_time
     hst_adjustment = datetime.timedelta(hours=-10)
     scoreboard_update_time = datetime.datetime.now(datetime.timezone.utc) + hst_adjustment
-    flask.flash("Scoreboard is current as of {}".format(scoreboard_update_time.strftime("%B %d, %H:%M")), "info")
+    update_time_string = scoreboard_update_time.strftime("%B %d, %H:%M HST")
+    flask.flash("Scoreboard is current as of {}".format(update_time_string), "info")
 
     return None
 
@@ -80,10 +82,7 @@ def welcome():
     Called by default. Flashes Lightning Labs logo then redirects to info page.
     """
     print("Loading welcome page...")
-    # update_scoreboard()  # DO NOT commit this line, this calls database for ~400 queries. debugging purposes only
-    # Automatically goes to home() after 0.5 seconds
     flask.flash("This website was created by Lightning Labs!", "info")
-
     return flask.render_template('Welcome.html')
 
 
@@ -92,16 +91,17 @@ def home():
     """
     This page displays scoreboard.
     """
-    if scoreboard_update_time == "N/A":
-        update_scoreboard()  # update scoreboard if it hasn't been updated
+    scoreboard_is_initialized = scoreboard_update_time.year == 1970
+    if scoreboard_is_initialized:
         print("Updating Scoreboard...")
+        update_scoreboard()  # update scoreboard if it hasn't been updated
+        
+    scoreboard_render: list[list] = scoreboard_global  # import scoreboard_global to be rendered
+    scoreboard_render_pivot: list[list] = scoreboard_global_pivot
+    show_scoreboard: bool = True
+    show_leaderboard: bool = True
 
-    scoreboard_render = scoreboard_global
-    scoreboard_render_pivot = scoreboard_global_pivot
-    show_scoreboard = True
-
-    # extract team name and scores into an array of dictionaries
-    teams = []
+    teams: list[dict] = []
     for row in scoreboard_render:
         row_element = {"name": row[0], "score": row[-1]}
         teams.append(row_element)
@@ -110,55 +110,55 @@ def home():
     if len(teams) > 0:
         teams.pop(0)
 
-    teams = sorted(teams, key=lambda x: -x["score"])  # Sort teams list by score, descending
+    # Sort teams list by score, descending
+    teams = sorted(teams, key=lambda x: -x["score"])
 
     # make list of events
-    events = []
+    events: list = []
     if len(scoreboard_render) > 0:
         events = scoreboard_render[0]
 
-    request_type = "NONE"
-    requested_info = {}
+    scoreboard_mini: dict = {}
     if flask.request.method == 'POST':  # if unit or event is selected
-        def req_code_parser(req_code):
-            return req_code.split("_")[1].replace("+", " ")  # removes header, and replaces "+" with " "
+        def req_code_parser(req_code) -> str:
+            """
+            Takes request code and converts it to unit or event name
+            """
+            return req_code.split("_")[1].replace("+", " ")
+
+        def make_scoreboard_mini(scoreboard):
+            table_name = req_code_parser(flask.request.form['request_code'])
+
+            # search for event or team name in scoreboard and insert info in to that row
+            req_info = [scoreboard[0]]
+            for scoreboard_row in scoreboard:
+                if scoreboard_row[0] == table_name:
+                    req_info.append(scoreboard_row)
+                    break
+
+            for index in range(len(req_info[0])):
+                scoreboard_mini[req_info[0][index]] = req_info[1][index]
 
         if flask.request.form['request_code'].startswith("TEAM_"):
+            make_scoreboard_mini(scoreboard=scoreboard_render)
+            show_leaderboard = False
             show_scoreboard = False
-            request_type = "UNIT"
-            parsed = req_code_parser(flask.request.form['request_code'])
-
-            req_info = [scoreboard_render[0]]  # header
-            for row in scoreboard_render:
-                if row[0] == parsed:  # match found
-                    req_info.append(row)
-                    break
-
-            for index in range(len(req_info[0])):
-                requested_info[req_info[0][index]] = req_info[1][index]
 
         if flask.request.form['request_code'].startswith("EVENT_"):
+            make_scoreboard_mini(scoreboard=scoreboard_render_pivot)
+            show_leaderboard = False
             show_scoreboard = False
-            request_type = "EVENT"
-            parsed = req_code_parser(flask.request.form['request_code'])
 
-            req_info = [scoreboard_render_pivot[0]]  # header
-            for row in scoreboard_render_pivot:
-                if row[0] == parsed:  # match found
-                    req_info.append(row)
-                    break
-
-            for index in range(len(req_info[0])):
-                requested_info[req_info[0][index]] = req_info[1][index]
-
+    scoreboard_update_time_string = scoreboard_update_time.strftime("%B %d, %H:%M HST")
+    flask.flash("Scoreboard is current as of {}".format(scoreboard_update_time_string), "info")
     return flask.render_template(
         'index.html',
         teams=teams,
         scoreboard=scoreboard_render_pivot,
         events=events,
-        request_type=request_type,
-        requested_info=requested_info,
-        show_scoreboard=show_scoreboard)
+        requested_info=scoreboard_mini,
+        show_scoreboard=show_scoreboard,
+        show_leaderboard=show_leaderboard)
 
 
 @app.route('/info', methods=['POST', 'GET'])
@@ -199,29 +199,36 @@ def account():
     """
 
     if flask.request.method == 'POST':  # called when a form submit is clicked
-        event_id = flask.request.form['event_id']
-        if event_id == "-1":  # logout code
+        event_id: str = flask.request.form['event_id']
+
+        logout_code = -1
+        admin_code = 0
+
+        if event_id == logout_code:
             flask.flash("Logged Out", "warning")
             flask.session.pop('user_code', None)
             return flask.redirect('home')
 
-        if event_id == "0":  # admin code
+        if event_id == admin_code:
             return flask.redirect('admin')
+
         else:
             flask.session['event_id'] = event_id
             return flask.redirect('edit')
 
-    events_access = {}  # dictionary of events, and whether the user has access
+    # see what access the user has and only display relevant buttons
+    events_access = {}
     events = models.Event.query.order_by(models.Event.id)
     user_code = flask.session['user_code']
     user_id = models.User.query.filter_by(code=user_code).first().id
     access_list = models.Access.query.filter_by(user_id=user_id)
-
     for access in access_list:
         events_access.update({access.event_id: True})
-        # add all access to dictionary
 
-    return flask.render_template('account.html', user_code=user_code, events_access=events_access, events=events)
+    return flask.render_template('account.html',
+                                 user_code=user_code,
+                                 events_access=events_access,
+                                 events=events)
 
 
 @app.route('/admin', methods=['POST', 'GET'])
@@ -230,7 +237,8 @@ def admin():
     Admin menu. Shows special actions available only to admin account holders.
     """
 
-    user_list = models.User.query.all()  # initialize user_list
+    # initialize user_dict
+    user_list = models.User.query.all()
     user_dict = {}
     for user in user_list:
         user_dict[user] = []
@@ -252,29 +260,26 @@ def admin():
 
         if request_code == "BACKUP_SCOREBOARD":
             write_to_csv("backup", "scoreboard.csv", scoreboard_global)
-            return flask.send_file(os.path.join("backup", "scoreboard.csv"), as_attachment=True)
+            backup_scoreboard = os.path.join("backup", "scoreboard.csv")
+            return flask.send_file(backup_scoreboard, as_attachment=True)
 
         if request_code == "BACKUP_DATABASE":
             backup_table_all()
-
-            zip_directory = zip_folder(os.path.join("backup", "database"))
-
-            flask.flash("Check backup/database folder", "success")
-            return flask.send_file(zip_directory, as_attachment=True)
+            backup_zip = zip_folder(os.path.join("backup", "database"))
+            return flask.send_file(backup_zip, as_attachment=True)
 
         if request_code.startswith("VIEWUSER_"):
-            """
-            call viewuser()
-            """
             view_object = models.User.query.filter_by(id=request_code.split("_")[1]).first()  # grab the User with the correct id
 
-            # load user info into sessions
+            # load user info into sessions then call viewuser()
             view_user = {"id": view_object.id, "code": view_object.code}
             flask.session['view_user'] = view_user
             return flask.redirect(url_for('viewuser'))
 
     flask.flash("This project cannot store PII such as Rank and Name. Contact Lightning Labs for more information.", 'danger')
-    return flask.render_template('admin.html', user_code=flask.session['user_code'], user_list=user_dict)
+    return flask.render_template('admin.html',
+                                 user_code=flask.session['user_code'],
+                                 user_list=user_dict)
 
 
 @app.route('/admin/viewuser', methods=['POST', 'GET'])
@@ -318,7 +323,9 @@ def viewuser():
 
     print(event_dict)
 
-    return flask.render_template('viewuser.html', view_user=view_user, event_dict=event_dict)
+    return flask.render_template('viewuser.html',
+                                 view_user=view_user,
+                                 event_dict=event_dict)
 
 
 @app.route('/edit', methods=['POST', 'GET'])
@@ -331,10 +338,16 @@ def edit():
     event_id = flask.session['event_id']
     event = models.Event.query.filter_by(id=event_id).first()
     teams = models.Team.query.order_by(models.Team.id)
+
+    # generate dropdown menu options for placement
     placements = []
     for i in range(teams.count()):
         placements.append((i + 1, num_to_ordinal(i + 1)))
-    return flask.render_template('edit.html', user_code=user_code, event=event, teams=teams, placements=placements)
+    return flask.render_template('edit.html',
+                                 user_code=user_code,
+                                 event=event,
+                                 teams=teams,
+                                 placements=placements)
 
 
 @app.route('/submit', methods=['POST', 'GET'])
