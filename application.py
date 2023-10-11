6aa60120e365a159d15c8ce8a1f346cf0c9d6533
Soomin Song <100112940+nimoooos@users.noreply.github.com>
2023-10-10 15:10:30 -1000

    waitress implementation
    Activating WSGI server for production

--- application.py
+++ application.py
@@ -4,7 +4,7 @@ from flask_debugtoolbar import DebugToolbarExtension
 import werkzeug.exceptions
 
 import env
-import lightning_labs
+import lightning_labs as LL
 import models
 from proj_util import random_user_code, pivot_table, write_to_csv, zip_folder, authorized
 
@@ -532,5 +532,7 @@ def submit():
 
 
 if __name__ == '__main__':
-    lightning_labs.print_logo()
-    app.run(host="0.0.0.0", debug=env.DEBUG)
+    LL.print_logo()
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=5000)
+    # app.run(host="0.0.0.0", debug=env.DEBUG)
