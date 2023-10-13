4261c7f1d8e8dd0f879ab4c2ab6a3e2860e6d205
Soomin Song <100112940+nimoooos@users.noreply.github.com>
2023-10-13 10:55:20 -1000

    Combined two tests in pivot table into one

--- unittest/test_proj_util.py
+++ unittest/test_proj_util.py
@@ -2,16 +2,12 @@ import unittest
 
 
 class TestCasePivotTable(unittest.TestCase):
-    def test_pivot_23(self):
+    def test_pivot(self):
         from proj_util import pivot_table
         array_23 = [[11, 12, 13], [21, 22, 23]]
         array_23_pivot = [[11, 21], [12, 22], [13, 23]]
         self.assertEqual(array_23, pivot_table(array_23_pivot))
-
-    def test_double_pivot(self):
-        from proj_util import pivot_table
-        array_2d = [[11, 12, 13, 14], [21, 22, 23, 24], [31, 32, 33, 34]]
-        self.assertEqual(array_2d, pivot_table(pivot_table(array_2d)))
+        self.assertEqual(array_23, pivot_table(pivot_table(array_23)))
 
 
 class TestCaseRandomCode(unittest.TestCase):
