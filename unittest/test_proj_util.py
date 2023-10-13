5acbd9495edae3d0610df7da6ca45b893025e577
Soomin Song <100112940+nimoooos@users.noreply.github.com>
2023-10-13 10:55:45 -1000

    CSV writing test
    checks for if the file was written, and if the file content is correct

--- unittest/test_proj_util.py
+++ unittest/test_proj_util.py
@@ -20,5 +20,39 @@ class TestCaseRandomCode(unittest.TestCase):
         self.assertNotIn(code, code_list)
 
 
+class TestCaseWriteCSV(unittest.TestCase):
+    def setUp(self):
+        from proj_util import write_to_csv
+        import os
+        self.directory = os.path.join("testfiles")
+        if not os.path.exists(self.directory):
+            os.makedirs(self.directory)
+
+        self.array_34 = [[11, 12, 13, 14], [21, 22, 23, 24], [31, 32, 33, 34]]
+        self.filename = "CSV_test_write.csv"
+        self.filepath = os.path.join(self.directory, self.filename)
+
+        write_to_csv(self.directory, self.filename, self.array_34)
+
+    def tearDown(self):
+        import os
+        if os.path.exists(self.filepath):
+            os.remove(self.filepath)
+
+    def test_write(self):
+        """
+        Checks if the file was written
+        """
+        from os import path
+        file_exists = path.isfile(self.filepath)
+        self.assertTrue(file_exists)
+
+    def test_read(self):
+        file = open(self.filepath, "r")
+        file_string = file.read()
+        self.assertEqual("11,12,13,14\n21,22,23,24\n31,32,33,34\n", file_string)
+        file.close()
+
+
 if __name__ == '__main__':
     unittest.main()
