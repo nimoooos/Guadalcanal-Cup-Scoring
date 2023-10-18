2bdda32603e83dfb7a47e203dce61613348d563b
Soomin Song <100112940+nimoooos@users.noreply.github.com>
2023-10-18 11:40:31 -1000

    Update lightning_labs.py
    Logo generation updated with adjustable speed

--- lightning_labs.py
+++ lightning_labs.py
@@ -35,8 +35,13 @@ image = ("                                                                      
          "                                                                                 \n")
 
 
-def print_logo():
+def print_logo(time: float= 0, matrix_mode: bool = False):
+    from time import sleep
+    IMAGE_LENGTH = 1207  # not counting blank space
+    SPEED = time/IMAGE_LENGTH
+
     for pixel in image:
+        speed = SPEED
         match pixel:
             case "*": print(Style.RESET_ALL+Back.YELLOW+Fore.LIGHTYELLOW_EX, end="")
             case "(": print(Style.RESET_ALL+Back.YELLOW+Fore.LIGHTYELLOW_EX, end="")
@@ -47,8 +52,17 @@ def print_logo():
             case "#": print(Style.RESET_ALL+Back.BLACK+Fore.RED+Style.BRIGHT, end="")
             case "+": print(Style.RESET_ALL+Back.BLACK+Fore.RED, end="")
             case "@": print(Style.RESET_ALL+Back.BLACK+Fore.LIGHTBLACK_EX, end="")
-            case " ": print(Style.RESET_ALL+Back.BLACK, end="")
+            case " ":
+                print(Style.RESET_ALL+Back.BLACK, end="")
+                speed = 0
             case "\n": print(Style.RESET_ALL, end="")
             case default: print(Style.RESET_ALL+Back.BLACK, end="")
+        if matrix_mode and pixel != "\n": print(Back.BLACK + Fore.LIGHTGREEN_EX, end="")
         print(pixel, end="")
+        sleep(speed)
     return None
+
+
+if __name__ == '__main__':
+    print_logo()
+    print_logo(time=10, matrix_mode=True)
