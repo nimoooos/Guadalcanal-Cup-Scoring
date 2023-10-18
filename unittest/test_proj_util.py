6649d454b96853b1cc9eb413f2236c1d61f43370
Soomin Song <100112940+nimoooos@users.noreply.github.com>
2023-10-18 13:40:58 -1000

    Update test_proj_util.py

--- unittest/test_proj_util.py
+++ unittest/test_proj_util.py
@@ -5,7 +5,7 @@ from proj_util import now_hst, random_code
 test_code = random_code(5)
 logging.basicConfig(level=logging.DEBUG,
                     format='{code} - {time} - %(levelname)s: %(message)s'.format(code=test_code, time=now_hst("string")),
-                    filename="unittest.log", filemode='a')
+                    filename=f"unittest-{test_code}.log", filemode='a')
 
 
 class TestCasePivotTable(unittest.TestCase):
@@ -32,7 +32,10 @@ class TestCaseRandomCode(unittest.TestCase):
         logging.debug("Generating a large list of random codes.")
         for i in range(65536):
             code_list.append(random_code(6))
-        self.assertNotIn(code, code_list)
+        if code in code_list:
+            self.assertNotIn(code, code_list)
+            logging.warning(f"{code} was repeated!")
+
         logging.info("test_randomness() completed.")
 
 
@@ -46,7 +49,7 @@ class TestCaseWriteCSV(unittest.TestCase):
             os.makedirs(self.directory)
 
         self.array_34 = [[11, 12, 13, 14], [21, 22, 23, 24], [31, 32, 33, 34]]
-        self.filename = "CSV_test_write.csv"
+        self.filename = f"{test_code}-CSV_test_write.csv"
         self.filepath = os.path.join(self.directory, self.filename)
 
         write_to_csv(self.directory, self.filename, self.array_34)
@@ -57,23 +60,23 @@ class TestCaseWriteCSV(unittest.TestCase):
         if os.path.exists(self.filepath):
             os.remove(self.filepath)
 
-    def test_write(self):
+    def test_csv_write(self):
         """
         Checks if the file was written
         """
-        logging.info("test_write() initialized.")
+        logging.info("test_csv_write() initialized.")
         from os import path
         file_exists = path.isfile(self.filepath)
         self.assertTrue(file_exists)
-        logging.info("test_write() completed.")
+        logging.info("test_csv_write() completed.")
 
-    def test_read(self):
-        logging.info("test_read() initialized.")
+    def test_csv_read(self):
+        logging.info("test_csv_read() initialized.")
         file = open(self.filepath, "r")
         file_string = file.read()
         self.assertEqual("11,12,13,14\n21,22,23,24\n31,32,33,34\n", file_string)
         file.close()
-        logging.info("test_read() completed.")
+        logging.info("test_csv_read() completed.")
 
 
 if __name__ == '__main__':
